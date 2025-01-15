f764a103959503c413c7d12111bdf4daf75b4bc3
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-15 13:23:24 -0500

    add do nothing terraform for ys1-staging testing only

--- solutions/instances/provider.tf
+++ solutions/instances/provider.tf
@@ -2,18 +2,7 @@
 # Provider config
 ########################################################################################################################
 
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = local.scc_instance_region
-  visibility       = var.provider_visibility
-}
 
-provider "ibm" {
-  alias            = "kms"
-  ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
-  region           = local.kms_region
-  visibility       = var.provider_visibility
-}
 
 provider "ibm" {
   alias            = "cos"
