c3f2f9a5e7994f21ff7c102a89b542e1b26e9bf8
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-03-13 14:30:25 -0400

    read ssh keys list to validate api key by doing a login. update da to match prod

--- solutions/instances/provider.tf
+++ solutions/instances/provider.tf
@@ -5,8 +5,28 @@
 
 
 provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = "us-south"
+  visibility       = var.provider_visibility
+}
+
+provider "ibm" {
+  alias            = "kms"
+  ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
+  region           = "us-south"
+  visibility       = var.provider_visibility
+}
+
+provider "ibm" {
   alias            = "cos"
   ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.cos_region
+  region           = "us-south"
+  visibility       = var.provider_visibility
+}
+
+provider "ibm" {
+  alias            = "en"
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = "us-south"
   visibility       = var.provider_visibility
 }
